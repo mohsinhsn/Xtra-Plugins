91fdd6eb365943f5687b209b5f491878f4120695
mohsinhsn <84607974+mohsinhsn@users.noreply.github.com>
2021-07-08 00:05:32 +0600

    Update Cricket_info.py

--- Cricket_info.py
+++ Cricket_info.py
@@ -1,8 +1,8 @@
-# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
+# Copyright (C) 2020-2021 by MohsinHsn@Github, < https://github.com/MohsinHsn >.
 #
-# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
+# This file is part of < https://github.com/MohsinHsn/StylishUserBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
+# Please see < https://github.com/MohsinHsn/blob/master/LICENSE >
 #
 # All rights reserved.
 
@@ -14,7 +14,7 @@ from pyrogram import filters
 from main_startup.core.decorators import friday_on_cmd
 
 
-@friday_on_cmd(
+@stylish_on_cmd(
     ["cs"],
     cmd_help={
         "help": "Get live cricket score info",
