64a85e867f8348421e2f7c56aa8d437184b4e860
mohsinhsn <84607974+mohsinhsn@users.noreply.github.com>
2021-07-17 13:16:09 +0600

    Update tag_alert.py

--- tag_alert.py
+++ tag_alert.py
@@ -1,12 +1,12 @@
-# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
+# Copyright (C) 2020-2021 by MohsinHsn@Github, < https://github.com/MohsinHsn >.
 #
-# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
+# This file is part of < https://github.com/MohsinHsn/StylishUserBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
+# Please see < https://github.com/MohsinHsn/blob/master/LICENSE >
 #
 # All rights reserved.
 
-from main_startup.core.decorators import friday_on_cmd, Config, listen
+from main_startup.core.decorators import stylish_on_cmd, Config, listen
 from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
 from main_startup import bot
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
