267562f9fd21426c3e0e78da92d604520a918760
mohsinhsn <84607974+mohsinhsn@users.noreply.github.com>
2021-07-17 13:13:22 +0600

    Update subtitles.py

--- subtitles.py
+++ subtitles.py
@@ -1,8 +1,8 @@
-# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
+# Copyright (C) 2020-2021 by MohsinHsn@Github, < https://github.com/MohsinHsn >.
 #
-# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
+# This file is part of < https://github.com/MohsinHsn/StylishUserBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
+# Please see < https://github.com/MohsinHsn/blob/master/LICENSE >
 #
 # All rights reserved.
 
@@ -13,7 +13,7 @@ import requests
 from bs4 import BeautifulSoup as bs
 import shutil
 from main_startup.config_var import Config
-from main_startup.core.decorators import friday_on_cmd
+from main_startup.core.decorators import stylish_on_cmd
 from main_startup.core.startup_helpers import run_cmd
 from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
 BASE_URL = "https://isubtitles.org"
@@ -67,7 +67,7 @@ async def get_lang(keyword):
                 link.append(f"{BASE_URL}{b.get('href')}")
     return index, language, link
 
-@friday_on_cmd(
+@stylish_on_cmd(
     ["subs", "substitle", "subdl"],
     cmd_help={
         "help": "Get Subtitle Of A Movie.",
