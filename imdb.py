1800dd49ad50211b2c2767ec06ded8b33742f24c
mohsinhsn <84607974+mohsinhsn@users.noreply.github.com>
2021-07-17 11:56:07 +0600

    Update imdb.py

--- imdb.py
+++ imdb.py
@@ -1,18 +1,18 @@
-# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
+# Copyright (C) 2020-2021 by MohsinHsn@Github, < https://github.com/MohsinHsn >.
 #
-# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
+# This file is part of < https://github.com/MohsinHsn/StylishUserBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
+# Please see < https://github.com/MohsinHsn/blob/master/LICENSE >
 #
 # All rights reserved.
 
-from main_startup.core.decorators import friday_on_cmd
+from main_startup.core.decorators import stylish_on_cmd
 from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
 import requests
 import bs4
 import re
 
-@friday_on_cmd(
+@stylish_on_cmd(
     ["imdb"],
     cmd_help={
         "help": "it provides details and ratings about given movie/show",
