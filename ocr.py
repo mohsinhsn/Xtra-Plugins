eedec5ce6a97916ef1ae36374a42f5ba99f58f7b
mohsinhsn <84607974+mohsinhsn@users.noreply.github.com>
2021-07-17 12:50:32 +0600

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -1,13 +1,13 @@
-# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
+# Copyright (C) 2020-2021 by MohsinHsn@Github, < https://github.com/MohsinHsn >.
 #
-# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
+# This file is part of < https://github.com/MohsinHsn/StylishUserBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
+# Please see < https://github.com/MohsinHsn/blob/master/LICENSE >
 #
 # All rights reserved.
 
 from main_startup.config_var import Config
-from main_startup.core.decorators import friday_on_cmd
+from main_startup.core.decorators import stylish_on_cmd
 from main_startup.core.startup_helpers import run_cmd
 from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
 from main_startup.helper_func.plugin_helpers import convert_to_image
@@ -35,7 +35,7 @@ async def read_ocr_(img_path):
     return response.json()["ParsedResults"][0]['ParsedText']
 
 
-@friday_on_cmd(
+@stylish_on_cmd(
     ["ocr"],
     cmd_help={
         "help": "Ocr Images.",
