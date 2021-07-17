80b3d3dd629e3217bef1be412435ea373b71bf2e
mohsinhsn <84607974+mohsinhsn@users.noreply.github.com>
2021-07-17 11:57:49 +0600

    Update iplookup.py

--- iplookup.py
+++ iplookup.py
@@ -1,18 +1,18 @@
-# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
+# Copyright (C) 2020-2021 by MohsinHsn@Github, < https://github.com/MohsinHsn >.
 #
-# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
+# This file is part of < https://github.com/MohsinHsn/StylishUserBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
+# Please see < https://github.com/MohsinHsn/blob/master/LICENSE >
 #
 # All rights reserved.
 
 from main_startup.config_var import Config
-from main_startup.core.decorators import friday_on_cmd
+from main_startup.core.decorators import stylish_on_cmd
 from main_startup.core.startup_helpers import run_cmd
 from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
 import aiohttp
 
-@friday_on_cmd(
+@stylish_on_cmd(
     ["ip"],
     cmd_help={
         "help": "Get Info About IP.",
