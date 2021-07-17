25e683b3236e4814e05202706446100800199bb3
mohsinhsn <84607974+mohsinhsn@users.noreply.github.com>
2021-07-17 12:22:51 +0600

    Update makemeazombie.py

--- makemeazombie.py
+++ makemeazombie.py
@@ -1,13 +1,13 @@
-# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
+# Copyright (C) 2020-2021 by MohsinHsn@Github, < https://github.com/MohsinHsn >.
 #
-# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
+# This file is part of < https://github.com/MohsinHsn/StylishUserBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
+# Please see < https://github.com/MohsinHsn/blob/master/LICENSE >
 #
 # All rights reserved.
 
 from main_startup.config_var import Config
-from main_startup.core.decorators import friday_on_cmd
+from main_startup.core.decorators import stylish_on_cmd
 from main_startup.core.startup_helpers import run_cmd
 from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
 from main_startup.helper_func.plugin_helpers import convert_to_image
@@ -25,13 +25,13 @@ async def make_me_a_zombie(img_path):
 
 def base64_to_image(base_code):
     """Decode And Convert To image Format."""
-    img_name = "zombie_by_FRIDAYUB.png"   
+    img_name = "zombie_by_StylishUB.png"   
     with open(img_name,"wb") as f:
         f.write(b64decode(str(base_code))) 
         f.close()
     return img_name
 
-@friday_on_cmd(
+@stylish_on_cmd(
     ["zombie"],
     cmd_help={
         "help": "Make A Person Look Like A Zombie.",
