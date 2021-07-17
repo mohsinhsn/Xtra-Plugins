eb9920e3c8fcc62af4ffd0047594a012ed881b21
mohsinhsn <84607974+mohsinhsn@users.noreply.github.com>
2021-07-17 12:40:27 +0600

    Update night_mode.py

--- night_mode.py
+++ night_mode.py
@@ -1,13 +1,13 @@
-# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
+# Copyright (C) 2020-2021 by MohsinHsn@Github, < https://github.com/MohsinHsn >.
 #
-# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
+# This file is part of < https://github.com/MohsinHsn/StylishUserBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
+# Please see < https://github.com/MohsinHsn/blob/master/LICENSE >
 #
 # All rights reserved.
 
-from main_startup.core.decorators import friday_on_cmd
-from main_startup import bot, Friday, Config
+from main_startup.core.decorators import stylish_on_cmd
+from main_startup import bot, Stylish, Config
 from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
 from xtraplugins.dB.nightmodedb import is_night_chat_in_db, get_all_night_chats, rm_night_chat, add_night_chat
 from pyrogram.types import ChatPermissions
@@ -15,7 +15,7 @@ from main_startup.helper_func.logger_s import LogIt
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import logging
 
-@friday_on_cmd(
+@stylish_on_cmd(
     ["scgrp"],
     is_official=False,
     only_if_admin=True,
@@ -35,7 +35,7 @@ async def scgrp(client, message):
     await pablo.edit(f"**Added Chat {message.chat.title} With Id {message.chat.id} To Database. This Group Will Be Closed On 12Am(IST) And Will Opened On 06Am(IST)**")
 
 
-@friday_on_cmd(
+@stylish_on_cmd(
     ["rsgrp"],
     is_official=False,
     only_if_admin=True,
@@ -52,7 +52,7 @@ async def scgrp(client, message):
         await pablo.edit("This Chat is Has Not Enabled Night Mode.")
         return
     await rm_night_chat(message.chat.id)
-    await pablo.edit(f"**Removed Chat {message.chat.title} With Id {message.chat.id} From Database. This Group Will Be No Longer Closed On 12Am(IST) And Will Opened On 06Am(IST)**")
+    await pablo.edit(f"**Removed Chat {message.chat.title} With Id {message.chat.id} From Database. This Group Will Be No Longer Closed On 12Am(BST) And Will Opened On 06Am(BST)**")
 
 
 async def job_close():
@@ -61,26 +61,26 @@ async def job_close():
         return
     for warner in lol:
         try:
-            await Friday.send_message(
-              int(warner.get("chat_id")), "`12:00 Am, Group Is Closing Till 6 Am. Night Mode Started !` \n**Powered By @FRidayOT**"
+            await Stylish.send_message(
+              int(warner.get("chat_id")), "`12:00 Am, Group Is Closing Till 6 Am. Night Mode Started !` \n**Powered By @StylishUser**"
             )
-            await Friday.set_chat_permissions(warner.get("chat_id"), ChatPermissions())
-            async for member in Friday.iter_chat_members(warner.get("chat_id")):
+            await Stylish.set_chat_permissions(warner.get("chat_id"), ChatPermissions())
+            async for member in Stylish.iter_chat_members(warner.get("chat_id")):
              if member.user.is_deleted:
                 try:
-                    await Friday.kick_chat_member(warner.get("chat_id"), member.user.id)
+                    await Stylish.kick_chat_member(warner.get("chat_id"), member.user.id)
                 except:
                     pass
         except Exception as e:
             logging.info(str(e))
             ido = warner.get("chat_id")
             try:
-                await Friday.send_message(Config.LOG_GRP, f"[NIGHT MODE]\n\nFailed To Close The Group {ido}.\nError : {e}")
+                await Stylish.send_message(Config.LOG_GRP, f"[NIGHT MODE]\n\nFailed To Close The Group {ido}.\nError : {e}")
             except:
                 pass
 
 
-scheduler = AsyncIOScheduler(timezone="Asia/Kolkata")
+scheduler = AsyncIOScheduler(timezone="Asia/Dhaka")
 scheduler.add_job(job_close, trigger="cron", hour=23, minute=55)
 scheduler.start()
 
@@ -90,10 +90,10 @@ async def job_open():
         return
     for warner in lol:
         try:
-            await Friday.send_message(
-              int(warner.get("chat_id")), "`06:00 Am, Group Is Opening.`\n**Powered By @FRidayOT**"
+            await Stylish.send_message(
+              int(warner.get("chat_id")), "`06:00 Am, Group Is Opening.`\n**Powered By @StylishUser**"
             )
-            await Friday.set_chat_permissions(
+            await Stylish.set_chat_permissions(
                         warner.get("chat_id"),
                         ChatPermissions(
                             can_send_messages=True,
