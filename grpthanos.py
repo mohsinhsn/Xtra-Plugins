24ce53c8f5e373d9bcda581c031c0fd4b44b554c
mohsinhsn <84607974+mohsinhsn@users.noreply.github.com>
2021-07-24 02:20:27 +0600

    Create grpthanos.py

---
+++ grpthanos.py
@@ -0,0 +1,44 @@
+import telethon 
+from telethon import functions
+from stylishbot.function import is_admin
+
+@stylish.on(stylish_on_cmd(pattern="(banall|snap)$"))
+async def sed(event):
+    if event.is_private:
+        await event.edit("`This Plugin Only Works In Groups Channel`")
+        return
+    user_s = 0
+    tries = 0
+    hmm = await bot.get_me()
+    if not await is_admin(event, hmm.id):
+        await event.edit("`You Should Be Admin To Do This !`")
+        return
+    async for user in event.client.iter_participants(int(event.chat_id)):
+        await event.edit(f"**Banning** \n**USER FIRST-NAME : ** `{user.first_name}` \n**USER ID :** `{user.id}`") 
+        try:
+            await event.client.edit_permissions(event.chat_id, user.id, view_messages=True)
+            tries += 1
+        except:
+            user_s += 1 
+    await event.edit(f"**Banned {user_s - tries} Users From This Group, Failed To Ban {user_s} Users**")
+
+@stylish.on(stylish_on_cmd(pattern="kickall$"))
+async def rip(event):
+    if event.is_private:
+        await event.edit("`This Plugin Only Works In Groups Channel`")
+        return
+    user_s = 0
+    tries = 0
+    hmm = await bot.get_me()
+    if not await is_admin(event, hmm.id):
+        await event.edit("`You Should Be Admin To Do This !`")
+        return
+    async for user in event.client.iter_participants(int(event.chat_id)):
+        if user.id != hmm.id:
+            await event.edit(f"**Kicking** \n**USER FIRST-NAME : ** `{user.first_name}` \n**USER ID :** `{user.id}`") 
+       	    try:
+                await event.client.kick_participant(event.chat_id, user.id)
+                tries += 1
+            except:
+                user_s += 1 
+    await event.edit(f"**Kicked {user_s - tries} Users From This Group, Failed To Kick {user_s} Users**")
